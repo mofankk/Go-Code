fcdb2afe7fa85a1e66a6f9deec1a30eaa80948e3
Youpeng <mofankk@gmail.com>
2021-07-20 22:58:59 +0800

    update

--- xgo/del.go
+++ xgo/del.go
@@ -10,6 +10,14 @@ func main() {
 
     a = b
 
+
+    aaa := getstr("hello")
+
     fmt.Printf("%T +  %v \n", a, a)
     fmt.Printf("%T  + %v \n", b, b)
+}
+
+
+func getstr(str string) string {
+	return "hello"
 }
