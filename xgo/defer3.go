c4d82a32c7f08c9b48238cd4114f87424a234536
Cui Youpeng <mofankk@gmail.com>
2022-03-14 09:51:57 +0800

    add defer demo

--- xgo/defer3.go
+++ xgo/defer3.go
@@ -2,19 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func test1() {
 	fmt.Println("test")
 }
 
-func test2() {
-	panic(1)
-}
-
 func main() {
 	fmt.Println("main start")
 	defer test1()
-	test2()
 	fmt.Println("main end")
+	os.Exit(0)
 }
